735c27890085c651daab8b165a26da2472e4bcb8
Jamiayn-Kh <jaik320024444@gmail.com>
2026-07-23 00:18:49 +0800

    db connection

--- src/test/java/mn/mungunurlal/BackendApplicationTests.java
+++ src/test/java/mn/mungunurlal/BackendApplicationTests.java
@@ -2,12 +2,14 @@ package mn.mungunurlal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void applicationContextLoadsWithoutProductionDatabase() {
 	}
 
 }
