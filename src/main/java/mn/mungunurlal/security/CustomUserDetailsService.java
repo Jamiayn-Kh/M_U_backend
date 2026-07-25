a09c43234b7fcb1c3cc95d0a8bd07673ae7be44c
Jamiayn-Kh <jaik320024444@gmail.com>
2026-07-25 23:02:13 +0800

    jwt token

---
+++ src/main/java/mn/mungunurlal/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package mn.mungunurlal.security;
+
+import mn.mungunurlal.user.domain.User;
+import mn.mungunurlal.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found")
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPasswordHash())
+                .roles(user.getRole().name())
+                .disabled(!user.isActive())
+                .build();
+    }
+}
